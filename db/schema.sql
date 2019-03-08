7a8284ac866774839761990152ef5e8591db45cb
nise406 <44655381+nise406@users.noreply.github.com>
2019-03-07 19:22:28 -0700

    commit

--- db/schema.sql
+++ db/schema.sql
@@ -16,6 +16,8 @@ PRIMARY KEY (id)
 
 );
 
+SELECT * FROM burgers;
+
 INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Triple-Stacked Avocado Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cali-Chicken Cheeseburger', false);
